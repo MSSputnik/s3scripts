561af104e92bfff3367a94fa46f5da2de903267e
Martin Schmidt <martin.schmidt@bea-services.com>
2019-04-21 11:27:53 +0200

    Add support for settings scripts to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-#Version=201810031320
+#Version=201904211015
 
+# known clientMode:
+# ro: get.sh list.sh 
+# wo: upload.sh delete.sh
+# sr: sget.sh slist.sh
+# sw: supload.sh sdelete.sh
+# sro: get.sh list.sh sget.sh slist.sh
+# swo: upload.sh delete.sh supload.sh slist.sh
+# srw: get.sh list.sh upload.sh delete.sh sget.sh slist.sh supload.sh sdelete.sh 
+# so: sget.sh slist.sh supload.sh sdelete.sh
+# default: get.sh list.sh upload.sh delete.sh
 clientRepo="https://raw.githubusercontent.com/MSSputnik/s3scripts/master"
 clientRFiles="get.sh list.sh"
 clientWFiles="upload.sh delete.sh"
+clientRSFiles="sget.sh slist.sh"
+clientWSFiles="supload.sh sdelete.sh"
 clientSFiles=
 echo "Installscript for s3scripts"
 clientType=$1
@@ -57,6 +69,24 @@ if [ $clientMode = "ro" ]; then
 fi
 if [ $clientMode = "wo" ]; then
   clientFiles="$clientWFiles"
+fi
+if [ $clientMode = "sr" ]; then
+  clientFiles="$clientRSFiles"
+fi
+if [ $clientMode = "sw" ]; then
+  clientFiles="$clientWSFiles"
+fi
+if [ $clientMode = "sro" ]; then
+  clientFiles="$clientRFiles $clientRSFiles"
+fi
+if [ $clientMode = "swo" ]; then
+  clientFiles="$clientWFiles $clientWSFiles"
+fi
+if [ $clientMode = "srw" ]; then
+  clientFiles="$clientRFiles $clientWFiles $clientRSFiles $clientWSFiles"
+fi
+if [ $clientMode = "so" ]; then
+  clientFiles="$clientRSFiles $clientWSFiles"
 fi
 if [ -z "$clientFiles" ]; then
   clientFiles="$clientRFiles $clientWFiles"
